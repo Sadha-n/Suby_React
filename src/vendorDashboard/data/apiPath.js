c74ebd9422a61090b335715ceae2949b6ad314ea
Sadha N <nsadha92@gmail.com>
2025-10-05 13:12:07 +0530

    chaneges path to live in api_url

--- src/vendorDashboard/data/apiPath.js
+++ src/vendorDashboard/data/apiPath.js
@@ -1,3 +1,3 @@
-export const API_URL="http://localhost:4000"
+// export const API_URL="http://localhost:4000"
 
-//  const API_URL="https://suby-backend-nodejs-6.onrender.com"
+ const API_URL="https://suby-backend-nodejs-6.onrender.com"
